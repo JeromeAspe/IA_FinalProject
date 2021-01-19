5edead17f81318ac6cc3c8665c9763faa4f9efa3
benjii66 <benjamin51.simon@gmail.com>
2021-01-19 14:37:13 +0100

    #2 player
    create a playerManager, script the player himself, and the shooter, add an interface for the shoot

---
+++ Assets/FinalProject/Benjamin/Scripts/Interface/IShooter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public interface IShooter 
+{
+    event Action OnShoot;
+    event Action OnShootHit;
+    float ShootDistance { get; }
+    float ReloadTimeValue { get; }
+    float FireRangeValue { get; }
+    int BulletsNumberMax { get; }
+    float Timer { get; }
+    void SetTimer();
+    void Shoot(bool _action);
+}
--- Assets/FinalProject/Benjamin/Scripts/Player/FP_Player.cs
+++ Assets/FinalProject/Benjamin/Scripts/Player/FP_Player.cs
@@ -2,17 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FP_Player : MonoBehaviour
+public class FP_Player : MonoBehaviour, IHandledItem<int>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] int id = 0;
+	[SerializeField] bool isEnable = true;
+	[SerializeField] FP_CameraSettings playerCameraSettings = new FP_CameraSettings();
+	public int ID => id;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public bool IsValid => true;
+	public bool IsEnabled => isEnable;
+	public Vector3 PlayerPosition => transform.position;
+	public Vector3 CameraPosition => playerCameraSettings.TargetPosition + playerCameraSettings.Offset;
+
+	public void SetID(int _id) => id = _id;
+	public void Disable()
+	{
+		isEnable = false;
+		FP_CameraManager.Instance?.Disable($"Player {ID}");
+	}
+
+	public void Enable()
+	{
+		isEnable = true;
+		FP_CameraManager.Instance?.Enable($"Player {ID}");
+	}
+
+	public void InitHandledItem()
+	{
+		InitCamera();
+		FP_PlayerManager.Instance?.Add(this);
+	}
+
+	void InitCamera()
+	{
+		//FP_CameraManager.Instance?.CreateCamera<FP_CameraFPSBehaviour>($"Player{ID}", playerCameraSettings, transform);
+
+	}
+
+	public void RemoveHandledItem()
+	{
+		FP_PlayerManager.Instance?.Remove(this);
+	}
+
+	void OnDrawGizmos()
+	{
+		if (Application.isPlaying) return;
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireCube(CameraPosition, Vector3.one / 2);
+		Gizmos.DrawLine(PlayerPosition, CameraPosition);
+	}
 }
