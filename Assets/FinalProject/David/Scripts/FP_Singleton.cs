d239fa430fcfeb5afe62a7a2828869082e4c1fd4
Puurple <57531220+Puurplle@users.noreply.github.com>
2021-01-19 11:24:05 +0100

    First push, hierarchy + Singleton

---
+++ Assets/FinalProject/David/Scripts/FP_Singleton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public abstract class FP_Singleton<T> : MonoBehaviour where T:MonoBehaviour
+{
+    static T instance = default(T);
+
+    [SerializeField, Header("Visual Debug Height"), Range(-100, 100)] float visualDebugHeight = 0;
+    [SerializeField, Header("Visual Debug Size"), Range(0, 100)] float visualDebugSize = 0.3f;
+    [SerializeField, Header("Visual Debug Color")] Color validColor = Color.green;
+    public static T Instance => instance;
+
+    public virtual bool IsValid => instance;
+    public virtual void Awake()
+    {
+        if(instance && instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        instance = this as T;
+        instance.name += $"[{instance.GetType().Name} MANAGER]";
+    }
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = IsValid ? validColor : Color.gray;
+        Vector3 _upPosition = transform.position + Vector3.up * visualDebugHeight;
+        Gizmos.DrawLine(transform.position, _upPosition);
+        Gizmos.DrawSphere(_upPosition, visualDebugSize);
+    }
+}
