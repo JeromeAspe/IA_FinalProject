de94cafe0479c95b4a9e754cdc036e7711882735
benjii66 <benjamin51.simon@gmail.com>
2021-01-19 12:51:21 +0100

    Add some PlayerScripts
    Add player movement, shoter, and the player himself
    Start to script the movement

---
+++ Assets/FinalProject/Benjamin/Scripts/Player/FP_PlayerMovement.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(CharacterController))]
+public class FP_PlayerMovement : MonoBehaviour
+{
+	public event Action OnUpdatePlayer = null;
+
+
+	#region Serialize Fields
+	[SerializeField] CharacterController controller = null;
+	[SerializeField] bool useClampValue = true;
+	[SerializeField] float clampMaxValue = 60, clampMinValue = 20;
+	[SerializeField, Range(0, 10)] float moveSpeed = 4;
+	[SerializeField, Range(0, 10)] float rotateSpeed = 2;
+
+	[SerializeField] FP_PlayerShooter shooter = null;
+	#endregion
+
+	FP_Player player = null;
+	Vector3 targetPosition = Vector3.zero;
+	Vector3 currentPosition => transform.position;
+	float horizontal = 0, vertical = 0;
+
+	float rotateY = 0, rotateX = 0;
+
+	public bool IsValid => shooter;
+
+	private void Awake()
+	{
+		OnUpdatePlayer += () =>
+		{
+			MoveTo();
+			RotateTo();
+		};
+
+	}
+
+	private void Update()
+	{
+		OnUpdatePlayer?.Invoke();
+	}
+
+	void Init()
+	{
+		if (!shooter) shooter = GetComponent<FP_PlayerShooter>();
+		player = GetComponent<FP_Player>();
+		//inputManager.Instance?.RegisterAxis(AxisAction.HorizontalMove, UpdateHorizontalMove); 
+		//inputManager.Instance?.RegisterAxis(AxisAction.VerticalMove, UpdateVerticalMove); 
+		//inputManager.Instance?.RegisterAxis(AxisAction.HorizontalAxis, UpdateHorizontalRotate); 
+		//inputManager.Instance?.RegisterAxis(AxisAction.VerticalAxis, UpdateVerticalRotate); 
+		//inputManager.Instance?.RegisterButton(ButtonAction.Fire,(fire) => shooter.Shoot(fire)); 
+		//inputManager.Instance?.RegisterButton(ButtonAction.Reload,(reload) => shooter.Reload(reload)); 
+		controller = GetComponent<CharacterController>();
+	}
+
+	#region Update Movement and Rotation
+
+	void UpdateHorizontalMove(float _h) => horizontal = _h * moveSpeed;
+
+	void UpdateVerticalMove(float _v) => vertical = _v * moveSpeed;
+
+	void UpdateHorizontalAxis(float _h)
+	{
+		rotateY += _h * rotateSpeed;
+		rotateY %= 360;
+	}
+
+	void UpdateVerticalAxis(float _v)
+	{
+		rotateX += _v * rotateSpeed;
+		float _clampValue = useClampValue ? clampMaxValue : 60;
+		rotateX = Mathf.Clamp(rotateX, -_clampValue, clampMinValue);
+	}
+	#endregion
+
+
+	#region Movement and Rotation
+	void RotateTo() => transform.eulerAngles = new Vector3(rotateX, rotateY, 0);
+
+	void MoveTo()
+	{
+		targetPosition = (transform.right * horizontal) + (transform.forward * vertical);
+		targetPosition.y -= 9.81f; //la gravité
+		controller.Move(targetPosition * Time.deltaTime);
+	}
+	#endregion
+
+
+
+	private void OnDestroy()
+	{
+		OnUpdatePlayer -= () =>
+		{
+			MoveTo();
+			RotateTo();
+		};
+		//inputManager.Instance?.UnRegisterAxis(AxisAction.HorizontalMove, UpdateHorizontalMove); 
+		//inputManager.Instance?.UnRegisterAxis(AxisAction.VerticalMove, UpdateVerticalMove); 
+		//inputManager.Instance?.UnRegisterAxis(AxisAction.HorizontalAxis, UpdateHorizontalRotate); 
+		//inputManager.Instance?.UnRegisterAxis(AxisAction.VerticalAxis, UpdateVerticalRotate); 
+		//inputManager.Instance?.UnRegisterButton(ButtonAction.Fire,(fire) => shooter.Shoot(fire)); 
+		//inputManager.Instance?.UnRegisterButton(ButtonAction.Reload,(reload) => shooter.Reload(reload)); 
+	}
+	#region Gizmos
+	private void OnDrawGizmos()
+	{
+		DebugPositionMovement();
+		DebugRotation();
+	}
+
+
+	void DebugPositionMovement()
+	{
+		Vector3 _movement = currentPosition + new Vector3(targetPosition.x, 0, targetPosition.z);
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawSphere(_movement, .3f);
+		Gizmos.DrawLine(currentPosition, _movement);
+	}
+
+	void DebugRotation()
+	{
+		Gizmos.color = Color.green;
+		Vector3 _forwardAxis = transform.position + transform.forward * 2;
+		Gizmos.DrawSphere(_forwardAxis, .1f);
+		Gizmos.DrawLine(transform.position, _forwardAxis);
+	}
+	#endregion
+}
