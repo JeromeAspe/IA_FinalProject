8018471b2f1e213835688d4b588c84cce0defbfd
Jerome ASPE <aspe.jerome@gmail.com>
2021-01-19 12:29:19 +0100

    #4 ia movement

--- Assets/FinalProject/Jerome/Scripts/IA/Behaviours/FP_IAMovement.cs
+++ Assets/FinalProject/Jerome/Scripts/IA/Behaviours/FP_IAMovement.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.AI;
 
 public class FP_IAMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public event Action<bool> OnMove = null;
+    public event Action OnTargetReached = null;
+
+    [SerializeField] Vector3 moveTarget = Vector3.zero;
+    [SerializeField,Range(0,10)] float isAtPosDistance = 1;
+    [SerializeField,Range(0,20)] float moveSpeed = 2;
+    [SerializeField,Range(0,100)] float rotateSpeed = 10;
+
+    
+    public void SetMoveTarget(Vector3 _target)
     {
-        
+        moveTarget = _target;
+    }
+    public void MoveTo()
+    {
+        if (IsAtRange())
+        {
+            OnTargetReached?.Invoke();
+            return;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, moveTarget, Time.deltaTime * moveSpeed);
+    }
+    public void RotateTo()
+    {
+        Quaternion _angle = Quaternion.LookRotation(transform.position, moveTarget);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, _angle,Time.deltaTime*rotateSpeed);
+    }
+    public bool IsAtRange()
+    {
+        return Vector3.Distance(moveTarget, transform.position) < isAtPosDistance;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
