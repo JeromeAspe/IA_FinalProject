81e102b43b72216f6861205753aa5e363483dcb4
Jerome ASPE <aspe.jerome@gmail.com>
2021-01-19 16:04:27 +0100

    #11 obstacle

--- Assets/FinalProject/Jerome/Scripts/IA/Behaviours/FP_ObstacleTest.cs
+++ Assets/FinalProject/Jerome/Scripts/IA/Behaviours/FP_ObstacleTest.cs
@@ -16,24 +16,34 @@ public class FP_ObstacleTest : MonoBehaviour
     }
     private void Update()
     {
-        GetSideHidden();
+        Debug.Log(GetBestCoverSide().name);
     }
+    
     Vector3 GetNormalizedVector(Vector3 _position)
     {
         return new Vector3(_position.x, transform.position.y, _position.z);
     }
-    void GetSideHidden()
+    FP_CoverSite GetBestCoverSide()
     {
+        float _angle = 180;
+        FP_CoverSite _bestCover = null;
         for(int i = 0; i < covers.Length; i++)
         {
-            GetAngle(covers[i]);
+            float _coverAngle = GetAngle(covers[i]);
+            if (_coverAngle < _angle)
+            {
+                _bestCover = covers[i];
+                _angle = _coverAngle;
+            }
         }
+        //Debug.Log(_angle);
+        return _bestCover;
     }
     float GetAngle(FP_CoverSite _cover)
     {
         Vector3 _dir = GetNormalizedVector(_cover.transform.position) - TargetPos;
-        Debug.Log(Mathf.Abs(Vector3.Angle(_cover.transform.forward,_dir)));
-        return Mathf.Abs(Vector3.Angle(GetNormalizedVector(_cover.transform.position), TargetPos));
+        //Debug.Log(Mathf.Abs(Vector3.Angle(_cover.transform.forward,_dir)));
+        return Mathf.Abs(Vector3.Angle(_cover.transform.forward, _dir));
     }
     private void OnDrawGizmos()
     {
